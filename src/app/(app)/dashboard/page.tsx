e7dfc8b1279ce8bdd6bfa2721ca1431b89831bba
SugamChaudharry <nextgenachieverss@gmail.com>
2024-08-26 09:43:13 +0530

    chore: Update dependencies and add UI component for switch

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { useToast } from '@/components/ui/use-toast'
+import { Message } from '@/model/User'
+import React, { useState } from 'react'
 
 function page() {
+    const [messages, setMessages] = useState<Message[]>([])
+    const [loding, setLoding] = useState(false)
+    const [isSwitchLoding, setIsSwitchLoding] = useState(false)
+
+    const {toast} = useToast()
+
   return (
-    <div>page</div>
+    <div>User Dashboard</div>
   )
 }
+
+export default page
